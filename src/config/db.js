5ff1acd98c2f933542b35e41f920ac818aa00961
Williams Hernández <hernandw@gmail.com>
2026-03-17 20:44:44 -0300

    :sparkles: configuracion del .env. plantillla

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,25 @@
-import pg from 'pg'
+import pg from "pg";
+import 'dotenv/config'
+const { Pool } = pg;
 
-const { Pool } = pg
-
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME} = process.env
-
-
-
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
 
 const config = {
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    allowExitOnIdle: true
-}
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  allowExitOnIdle: true,
+};
+
+const pool = new Pool(config);
+
+/* const getDate = async () => {
+  const {rows} = await pool.query("select now()");
+  console.log(rows[0].now);
+};
+
+getDate();
+ */
 
