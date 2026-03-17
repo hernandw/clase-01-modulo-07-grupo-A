5417c03c0d6f49ef52e01ac96d8dae9d828d2e39
Williams Hernández <hernandw@gmail.com>
2026-03-17 20:04:34 -0300

    :sparkles: configurar variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,17 @@ import pg from 'pg'
 
 const { Pool } = pg
 
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME} = process.env
+
+
+
 
 const config = {
-    user: '',
-    host: '',
-    database: '',
-    password: '',
-    port: 5432,
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
     allowExitOnIdle: true
 }
+
